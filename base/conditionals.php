93b8086e4bae81dd227405e391089c782a027836
Giovanny Gonzalez <henrygiovanny.gonzalezwaltero@mercadolibre.com.co>
2021-05-20 18:54:40 -0500

    condicionales if

---
+++ base/conditionals.php
@@ -0,0 +1,51 @@
+<?php
+$x = 10;
+$y = '10';
+
+if ($x == $y) {
+  echo 'son iguales';
+}
+
+echo PHP_EOL;
+
+if ($x === $y) {
+  echo 'son identicos';
+} else {
+  echo 'NO son identicos';
+}
+
+echo PHP_EOL;
+
+if ($x != $y) {
+  echo 'Si son diferentes';
+}
+
+if ($x !== $y) {
+  echo 'Si son diferentes de identicos';
+}
+
+echo PHP_EOL;
+
+if ($x > $y) {
+  echo "$x es mayor que $y";
+} else if ($x < $y) {
+  echo "$x es menor que $y";
+} else if ($x == $y) {
+  echo "$x es igual que $y";
+} else {
+  echo "ninguna de las anteriores";
+}
+
+echo PHP_EOL;
+
+if ( ($x == $y) && ($x > 0) ) {
+  echo "$x es igual a $y y $x es mayor que 0";
+}
+
+echo PHP_EOL;
+
+if ( ($x == $y) || ($x === $y) ) {
+  echo "$x es igual a $y ó $x es identico que $y";
+}
+
+?>
